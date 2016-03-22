aed687106b4d5440c1be3b98f016e5d511006132
phil <uniphil@gmail.com>
2016-03-22 00:00:15 -0400

    Add a component test!
    Assert some stuff about what should be in the component for different props
    
    This invariant-oriented tooling an be extended later to generate a "props space" of
    combinations of valid properties, and assert the invariants across those combinations.
    
    It doesn't solve component testing -- we need to get actions in on this for that. But it's a
    start.

---
+++ component-invariants.js
@@ -0,0 +1,40 @@
+const React = require('react');
+const ReactTestUtils = require('react-addons-test-utils');
+const sticky = require('@openride/sticky-test');
+const harness = require('./harness');
+
+const componentInvariants = (Component, defaultProps, invariants) => {
+  const setup = whenProps => {
+    const renderer = ReactTestUtils.createRenderer();
+    const props = Object.assign({}, defaultProps, whenProps);
+    const element = React.createElement(Component, props);
+    renderer.render(element);
+    const result = renderer.getRenderOutput();
+    return Promise.resolve([result]);
+  };
+
+  const test = harness((name, whenProps) => sticky.compose(
+    sticky.declare(name),
+    sticky.countAsserts(1),
+    sticky.withResource(
+      () => setup(whenProps),
+      () => null
+    ),
+    sticky.timeout(100)
+  ));
+
+  invariants.forEach(inv => {
+    const name = Component.displayName || Component.name;
+    const propsRepr = Object.keys(inv.when).map(k =>
+      `${k}={${JSON.stringify(inv.when[k])}}`).join(' ');
+    const testDeclaration = `<${name} ${propsRepr}>`;
+    const assertBody = (s => s
+      .slice(s.indexOf('=>') + '=>'.length)
+      .trim()
+    )(inv.assert.toString());
+    test(testDeclaration, inv.when, (assert, output) =>
+        assert(inv.assert(output), assertBody));
+  });
+};
+
+module.exports = componentInvariants;
---
+++ dom-contains.js
@@ -0,0 +1,21 @@
+const nodeAssert = require('assert');
+
+const domContains = (root, thing) => {
+  try {
+    nodeAssert.deepEqual(root, thing);
+    return true;
+  } catch (err) {
+    if (root.props.children) {
+      const children = root.props.children;
+      if (Array.isArray(children)) {
+        return children.some(child => domContains(child, thing));
+      } else {
+        return domContains(children, thing);
+      }
+    } else {
+      return false;
+    }
+  }
+};
+
+module.exports = domContains;
