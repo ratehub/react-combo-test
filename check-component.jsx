b5dba8e1ee129a5863b0b1469ae104a6063e8eed
phil <uniphil@gmail.com>
2016-06-09 23:51:07 -0400

    Add component test for approved booking requests
    It fails on the old version, woo!
    
    Since I rewrote most of checkComponent, I've temporarily just commented out the old component tests from the old api. They should be easy to port over, but, different commit...

--- check-component.jsx
+++ check-component.jsx
@@ -1,51 +1,7 @@
-const Immutable = require('immutable');
-const { PropTypes: {
-  any,
-  bool,
-  func,
-  node,
-  number,
-  string
-} } = require('react');
 const React = require('react');
 const ReactTestUtils = require('react-addons-test-utils');
 const sticky = require('@openride/sticky-test');
 const harness = require('./harness');
-
-
-const standardGenerators = Immutable.Map([
-  [any, () =>
-    null],
-  [bool, () =>
-    Math.random() < 0.5],
-  [func, () =>
-    () => null],
-  [node, () =>
-    'Strings are nodes'],
-  [number, () =>  // TODO specify types of numbers
-    Math.floor(Math.random() * 24)],
-  [string, () =>
-    Immutable.Range(0, Math.floor(Math.random() * 32))  // 0-31 chars
-      .map(() => String.fromCharCode(Math.floor(Math.random() * Math.pow(2, 16))))  // random unicode from 16 bits
-      .join('')]
-]);
-
-const requiredify = generators =>
-  generators.flatMap((generator, checker) => Immutable.Map([
-    [checker.isRequired, generator],
-    [checker, () => Math.random() < 0.5 ? generator() : undefined]
-  ]));
-
-
-const getProps = (propTypes, generators) =>
-  Object.keys(propTypes)
-    .map(name => console.log('g', name, generators.get(propTypes[name])) || ({
-      name,
-      value: generators.get(propTypes[name])()
-    }))
-    .reduce((a, { name, value }) => Object.assign(a, {
-      [name]: value
-    }), {});
 
 
 const test = harness((description) => sticky.compose(
@@ -76,50 +32,91 @@ const checkProps = tree => {
 };
 
 
-const checkComponent = (Component, invariants = [], extraGenerators = Immutable.Map()) => {
-  if (!Component.propTypes) {
-    throw new Error(`No propTypes to check for <${Component.displayName || Component.name || 'anonymous'}>`);
-  }
-  const generators = requiredify(standardGenerators.merge(extraGenerators));
-
-  test(`Check component <${Component.displayName || Component.name || 'anonymous'}>`, (assert, renderer) => {
-    let renderOk = true;
-    let invariantsOk = Immutable.Range(0, invariants.length)
-      .map(() => true)
-      .toList();
-
-    Immutable.Range(0, 50).forEach(() => {
-      const props = getProps(Component.propTypes, generators);
-      const repr = JSON.stringify(props);
-      try {
-        renderer.render((<Component {...props} />));
-      } catch (err) {
-        renderOk = false;
-        assert.fail(`${String(err)} | props: ${repr}`);
+const checkPropValues = (propTypes, propValues, name) => {
+  for (const prop of Object.keys(propTypes)) {
+    for (const value of propValues[prop]) {
+      const result = propTypes[prop]({ prop: value }, 'prop', name, 'prop');
+      if (result) {
+        return result;
       }
-      let result;
-      try {
-        result = renderer.getRenderOutput();
-      } catch (err) {
-        assert.fail(`failed (throwing); props: ${repr}`);
-        throw err;
-      }
-      checkProps(result);
-      invariants.forEach(({ description, check }, i) => {
-        if (invariantsOk.get(i) && !check(props, result)) {
-          invariantsOk = invariantsOk.set(i, false);
-          assert.fail(`Invariant violation: ${description} | props: ${props}`);
-        }
-      });
-    });
-    if (renderOk) {
-      assert.pass(`Rendered 50 sets of props without throwing`);
     }
-    invariants
-      .filter((inv, i) => invariantsOk.get(i))
-      .forEach(({ description }) =>
-        assert.pass(`Invariant held: ${description}`));
-  });
+  }
 };
+
+
+const mult = (vec, vecs) => vec
+  .map(v => vecs.map(vv => [v].concat(vv)))  // prepend v to every vec
+  .reduce((a, b) => a.concat(b));  // join all the [[]]s
+
+
+const combos = arrayOfArrays => {
+  const [x, ...xs] = arrayOfArrays;
+  if (!x) {
+    return [];
+  } else if (!xs.length) {
+    return x.map(v => [v]);
+  } else {
+    return mult(x, combos(xs));
+  }
+};
+
+
+const zipObj = (keys, values) => {
+  const out = {};
+  keys.forEach((k, i) => out[k] = values[i]);
+  return out;
+};
+
+
+const propCombos = props => {
+  const keys = Object.keys(props);
+  const values = keys.map(k => props[k]);
+  const valueCombos = combos(values);
+  return valueCombos.map(combo => zipObj(keys, combo));
+};
+
+
+const checkComponent = (Component, {
+  props: propValues
+}) => test(`<${Component.displayName || Component.name} /> renders without throwing`, (assert, renderer) => {
+
+  let renderOk = true;
+  const name = Component.displayName || Component.name || 'UnnamedComponent';
+
+  if (!Component.propTypes) {
+    throw new Error(`No propTypes to check for <${name} />`);
+  }
+
+  const propValueErr = checkPropValues(Component.propTypes, propValues);
+  if (propValueErr) {
+    assert.fail(`Bad prop value: ${String(propValueErr)}`);
+  }
+
+  const cs = propCombos(propValues);
+  if (cs.length === 0) {
+    renderOk = false;
+    assert.fail('no prop combos to try');
+  }
+
+  for (const props of cs) {
+    try {
+      renderer.render((<Component {...props} />));
+    } catch (err) {
+      renderOk = false;
+      assert.fail(`${String(err)} | props: ${JSON.stringify(props)}`);
+      continue;
+    }
+    try {
+      checkProps(renderer.getRenderOutput());
+    } catch (err) {
+      renderOk = false;
+      assert.fail(`${String(err)} | props: ${JSON.stringify(props)}`);
+    }
+  }
+  if (renderOk) {
+    assert.pass(`Rendered ${cs.length} combinations of props for ${name} without throwing`);
+  }
+});
+
 
 module.exports = checkComponent;
