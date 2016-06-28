9b6ddc988997ff9c58b52961d32d2556a4b38f6e
Owen Scott <owen.m.scott@gmail.com>
2016-06-28 13:15:24 -0700

    fix getRide for reviews and add tests

--- util/check-prop-values.js
+++ util/check-prop-values.js
@@ -1,13 +1,13 @@
+
 const checkPropValues = (propTypes, propValues, name) => {
   for (const prop of Object.keys(propTypes)) {
     for (const value of propValues[prop]) {
-      const result = propTypes[prop]({ prop: value }, 'prop', name, 'prop');
-      if (result) {
-        return result;
+      const propValueError = propTypes[prop]({ prop: value }, 'prop', name, 'prop');
+      if (propValueError) {
+        return `Err: ${String(propValueError)} | PropName: ${name} | Value: ${value.toString()}`;
       }
     }
   }
 };
-
 
 export default checkPropValues;
