61c5510be75cf5e02fce2dd524971bda386cb1b8
phil <phil@ratehub.ca>
2017-07-06 11:30:08 -0400

    Move prop-combos into getCombos

--- src/__tests__/test-getCombos.js
+++ src/__tests__/test-getCombos.js
@@ -1,38 +1,39 @@
 const test = require('tape');
-const propCombos = require('../prop-combos');
+const getCombos = require('../getCombos');
 
 
-test('propCombos should throw if any prop has zero values', assert => {
+test('getCombos should throw if any prop has zero values', assert => {
   assert.plan(1);
-  assert.throws(() => propCombos({ a: [] }), 'should throw for empty prop value array');
+  assert.throws(() => getCombos({ a: [] }),
+    'should throw for empty prop value array');
 });
 
 
-test('propCombos should make all the combos', assert => {
+test('getCombos should make all the combos', assert => {
   assert.plan(6);
 
   assert.deepEqual(
-    propCombos({}),
+    getCombos({}),
     [],
     'shoudl create zero combos if there are no props');
 
   assert.deepEqual(
-    propCombos({ a: [1] }),
+    getCombos({ a: [1] }),
     [{ a: 1 }],
     'should work for a single prop with one value');
 
   assert.deepEqual(
-    propCombos({ a: [1, 2] }),
+    getCombos({ a: [1, 2] }),
     [{ a: 1 }, { a: 2 }],
     'should work for two values on one prop');
 
   assert.deepEqual(
-    propCombos({ a: [1], b: ['x'] }),
+    getCombos({ a: [1], b: ['x'] }),
     [{ a: 1, b: 'x' }],
     'should do two props, one value');
 
   assert.deepEqual(
-    propCombos({ a: [1, 2], b: ['x'] }),
+    getCombos({ a: [1, 2], b: ['x'] }),
     [
       { a: 1, b: 'x' },
       { a: 2, b: 'x' }
@@ -40,7 +41,7 @@ test('propCombos should make all the combos', assert => {
     'should handle two props, multiple values');
 
   assert.deepEqual(
-    propCombos({ a: [1, 2], b: ['x', 'y'] }),
+    getCombos({ a: [1, 2], b: ['x', 'y'] }),
     [
       { a: 1, b: 'x' },
       { a: 1, b: 'y' },
